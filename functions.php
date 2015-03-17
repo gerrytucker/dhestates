acb735333873e1525b027382ad1fc0ec7d4e4163
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-17 22:06:15 +0000

    Auto-generated commit 17/03/2015 10:06:08 PM

--- functions.php
+++ functions.php
@@ -5,14 +5,14 @@
  */
 function enqueue_theme_scripts() {
 
-  wp_register_script( 'modernizr', get_template_directory_uri() . '/scripts/modernizr.min.js', false, null, false );
+  wp_register_script( 'modernizr', get_template_directory_uri() . '/modernizr.min.js', false, null, false );
   wp_enqueue_script( 'modernizr' );
 
 	wp_deregister_script( 'jquery' );
   wp_register_script( 'jquery', ( '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js' ), false, null, true );
   wp_enqueue_script( 'jquery' );
 	
-	wp_register_script( 'app', get_template_directory_uri() . '/scripts/app.min.js', false, null, true );
+	wp_register_script( 'app', get_template_directory_uri() . '/app.min.js', false, null, true );
   wp_enqueue_script( 'app' );
 
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
